898469f2a0611eaa9b18ba11698cc9ff3cb0e4a1
Leonardo Santos <leocs.1991@gmail.com>
2014-01-07 19:07:10 -0200

    Fix #6

--- Gruntfile.js
+++ Gruntfile.js
@@ -17,7 +17,7 @@ module.exports = function(grunt) {
 	});
 
 	// 3. Where we tell Grunt we plan to use this plug-in.
-	grunt.loadNpmTasks('grunt-contrib-concat');
+	// grunt.loadNpmTasks('grunt-contrib-concat');
 	grunt.loadNpmTasks('grunt-contrib-uglify');
 
 	// 4. Where we tell Grunt what to do when we type "grunt" into the terminal.
