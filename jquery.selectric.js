8c9160f63c7991ac4bcb4b6536f405569b287a0a
Leonardo Santos <leocs.1991@gmail.com>
2015-06-03 11:29:07 -0300

    Update 2015-06-03T14:28:55.532Z

--- jquery.selectric.js
+++ jquery.selectric.js
@@ -9,7 +9,7 @@
  *    /,'
  *   /'
  *
- * Selectric Ϟ v1.9.2 (Apr 07 2015) - http://lcdsantos.github.io/jQuery-Selectric/
+ * Selectric Ϟ v1.9.3 (Jun 03 2015) - http://lcdsantos.github.io/jQuery-Selectric/
  *
  * Copyright (c) 2015 Leonardo Santos; Dual licensed: MIT/GPL
  *
